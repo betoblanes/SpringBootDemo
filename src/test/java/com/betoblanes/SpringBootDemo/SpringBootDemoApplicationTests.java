c0cb2009b52407cca3254fc81dc685a4012f1c8b
betoblanes <betoblanes@gmail.com>
2024-10-20 20:37:15 +0200

    Initialize Spring boot Project

---
+++ src/test/java/com/betoblanes/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betoblanes.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
